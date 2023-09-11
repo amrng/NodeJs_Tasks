40142b95b2679a1d28db682b395a00723a8c9d4d
Amr Elnagdy <107152234+amrng@users.noreply.github.com>
2023-09-11 14:03:38 +0300

    Delete 3- using mongoose_express_bcrypt_joi directory

--- 3- using mongoose_express_bcrypt_joi/day3/db/connection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const initConnection = () => {
-  mongoose
-    .connect("mongodb://127.0.0.1:27017/ITI_day3")
-    .then(console.log("DB connected"))
-    .catch((error) => console.log(error));
-};
